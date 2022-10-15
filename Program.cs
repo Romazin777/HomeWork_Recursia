00d02e502ec283c87d74377a61e9356431752ec1
Denis Romazin <romazin777@gmail.com>
2022-10-15 19:40:03 +0500

    Задача №68

--- Program.cs
+++ Program.cs
@@ -16,13 +16,31 @@
 // M = 1; N = 15 -> 120
 // M = 4; N = 8 -> 30
 
-Console.Write("Введите значение M: ");
+// Console.Write("Введите значение M: ");
+// int m = int.Parse(Console.ReadLine()!);
+// Console.Write("Введите значение N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.Write($"M = {m}; N = {n} -> {SumValueMN(m, n)}");
+// int SumValueMN(int m, int n)
+// {
+//         if (m == n + 1) return 0;
+//         return (m + SumValueMN(m + 1, n));
+// }
+
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+Console.Write("Введите положительное m: ");
 int m = int.Parse(Console.ReadLine()!);
-Console.Write("Введите значение N: ");
+Console.Write("Введите положительное число n: ");
 int n = int.Parse(Console.ReadLine()!);
-Console.Write($"M = {m}; N = {n} -> {SumValueMN(m, n)}");
-int SumValueMN(int m, int n)
+Console.Write($"m = {m}; m = {n} -> A(m,n) = {Akkerman(m, n)}");
+int Akkerman(int m, int n)
 {
-        if (m == n + 1) return 0;
-        return (m + SumValueMN(m + 1, n));
+    if (m == 0) return n + 1;
+    else if (( m > 0) && (n == 0)) return Akkerman(m - 1, 1);
+    else {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
 }
