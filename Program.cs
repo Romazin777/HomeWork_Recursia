64460a7ae9820dd3967b08cd72bb16f17449313d
Denis Romazin <romazin777@gmail.com>
2022-10-15 19:09:15 +0500

    Задача №66

--- Program.cs
+++ Program.cs
@@ -2,12 +2,27 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
+// Console.Write("Введите значение N: ");
+// int n = int.Parse(Console.ReadLine()!);
+// Console.WriteLine($"N = {n} -> {OrderingNumb(n)}");
+// string OrderingNumb(int number)
+// {
+//     if (number == 0) return string.Empty;
+//     if (number == 1) return ($"{number}" + OrderingNumb(number - 1));
+//     return ($"{number}" + ", " + OrderingNumb(number - 1));
+// }
+
+// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+Console.Write("Введите значение M: ");
+int m = int.Parse(Console.ReadLine()!);
 Console.Write("Введите значение N: ");
 int n = int.Parse(Console.ReadLine()!);
-Console.WriteLine($"N = {n} -> {OrderingNumb(n)}");
-string OrderingNumb(int number)
+Console.Write($"M = {m}; N = {n} -> {SumValueMN(m, n)}");
+int SumValueMN(int m, int n)
 {
-    if (number == 0) return string.Empty;
-    if (number == 1) return ($"{number}" + OrderingNumb(number - 1));
-    return ($"{number}" + ", " + OrderingNumb(number - 1));
+        if (m == n + 1) return 0;
+        return (m + SumValueMN(m + 1, n));
 }
