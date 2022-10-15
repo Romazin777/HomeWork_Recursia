c06b4e421cf4ea37540cd06da1c919621d84c190
Denis Romazin <romazin777@gmail.com>
2022-10-15 18:36:20 +0500

    Задача №64

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Write("Введите значение N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"N = {n} -> {OrderingNumb(n)}");
+string OrderingNumb(int number)
+{
+    if (number == 0) return string.Empty;
+    if (number == 1) return ($"{number}" + OrderingNumb(number - 1));
+    return ($"{number}" + ", " + OrderingNumb(number - 1));
+}
